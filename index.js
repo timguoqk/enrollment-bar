4f46c5dbd75e0be20ce457c606f7f7814fae4373
Tim Guo <timguoqk@gmail.com>
2015-02-08 13:29:24 -0800

    add cross major chart

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var rawData, data, ev;
-var keys = ["Graduate Total", "Professional Practice", "Interns and Residents", "Professional Masters", "Academic Masters", "Undergraduate", "Doctorate", "Total Campus"];
+var majors, keys = ["Graduate Total", "Professional Practice", "Interns and Residents", "Professional Masters", "Academic Masters", "Undergraduate", "Doctorate", "Total Campus"];
 var svg, x, y, colorScale, xAxis, yAxis;
 var margin = {top: 20, right: 30, bottom: 30, left: 40};
 var width = 900 - margin.left - margin.right;
@@ -9,21 +9,20 @@ $(function() {
 	$.getJSON('ucla_enrollment.json', function(data) {
 			rawData = data;
 			dict = []
-			var majors = _.keys(rawData);
+			majors = _.keys(rawData);
 			for (i = 0; i < majors.length; i ++)
 				dict.push({id:majors[i], text:majors[i]});
-			dict = _.sortBy(dict, function(x) {  return x.id;  });
+			dict = _.sortBy(dict, function(x) { return x.id; });
 			$("select#major-select").select2({data: dict});
-			$("select#major-select").on("select2:select", function (e) {  majorChangeTo(e.params.data.id);}  );
+			$("select#major-select").on("select2:select", function(e) { majorChangeTo(e.params.data.id);} );
 
 			// TODO: find an elegant solution
 			x = d3.scale.ordinal().rangeRoundBands([0, width], .1).domain(keys);
-			//y = d3.scale.linear().range([height, 0]).domain([0, 1898]);
 			y = d3.scale.linear().range([height, 0]);
 			colorScale = d3.scale.linear().range([45, 15]).domain([0, 1898]);  // 1898 is simply too big...
 
 			plotChartMajor('Computer Science');
-			//_.delay(1000, 'plotChartCrossMajor');  # TODO: uncomment after finishing up
+			plotChartCrossMajor();
 	});
 });
 
@@ -67,7 +66,7 @@ function plotChartMajor(key) {
   svg.selectAll(".bar")
       .data(keys)
     .enter().append("rect")
-      .attr("class", "bar fill-aqua")
+      .attr("class", "bar")
       .attr("x", function(d) { 
       	return x(d); 
       })
@@ -83,7 +82,6 @@ function plotChartMajor(key) {
 }
 
 function majorRandom() {
-	var majors = _.keys(rawData);
 	var key = majors[_.random(0, majors.length - 1)];
 	majorChangeTo(key);
 }
@@ -108,56 +106,59 @@ function majorChangeTo(key) {
 }
 
 function plotChartCrossMajor() {
-
-	// data = rawData[key];
-	// var values = _.values(data);
 	// Get only the total number
+	var num = _.map(rawData, function(val, key){ return val['Total Campus']; });
 
-	xAxis = d3.svg.axis().scale(x).orient("bottom");
-	yAxis = d3.svg.axis().scale(y).orient("left");
+	var x = d3.scale.ordinal().rangeRoundBands([0, width], .1).domain(majors);
+	var y = d3.scale.linear().range([height, 0]).domain([3, 1898]);
+	var colorScale = d3.scale.linear().range([45, 15]).domain([0, 1898]);
+	var xAxis = d3.svg.axis().scale(x).orient("bottom");
+	var yAxis = d3.svg.axis().scale(y).orient("left");
 
-	svg = d3.select("#chart-cross-majors").append("svg")
-	    .attr("width", width + margin.left + margin.right)
-	    .attr("height", height + margin.top + margin.bottom)
-	  .append("g")
-	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	var crossMajorSvg = d3.select("#chart-cross-majors").append("svg")
+	    									.attr("width", width + margin.left + margin.right)
+										    .attr("height", height + margin.top + margin.bottom)
+										  .append("g")
+										    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 	var tip = d3.tip()
 							.attr("class", "tooltip")
 							.offset([-10, 0])
 							.html(function(d) {
-								return "<span class=\"tooltip\">" + data[d] + "</span>";
+								return "<span class=\"tooltip\">" + d + ":" + rawData[d]['Total Campus'] + "</span>";
 							});
-	svg.call(tip);
+	crossMajorSvg.call(tip);
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+  // crossMajorSvg.append("g")
+		// 		      .attr("class", "x axis")
+		// 		      .attr("transform", "translate(0," + height + ")")
+		// 		      .call(xAxis);
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Students");
-  svg.selectAll(".bar")
-      .data(keys)
-    .enter().append("rect")
-      .attr("class", "bar fill-aqua")
-      .attr("x", function(d) { 
-      	return x(d); 
-      })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(data[d]); })
-      .attr("height", function(d) { return height - y(data[d]);})
-      .attr("fill", function(d) {
-      	return "hsla(177,100%," + colorScale(data[d]) + "%,1)";
-      	//return randomColor({luminosity: 'light'});
-      })
-      .on("mouseover", tip.show)
-      .on("mouseout", tip.hide);
+  crossMajorSvg.append("g")
+				      .attr("class", "y axis")
+				      .call(yAxis)
+				    .append("text")
+				      .attr("transform", "rotate(-90)")
+				      .attr("y", 6)
+				      .attr("dy", ".71em")
+				      .style("text-anchor", "end")
+				      .text("Students");
+
+  crossMajorSvg.selectAll(".bar")
+				      .data(majors)
+				    .enter().append("rect")
+				      .attr("class", "bar")
+				      .attr("x", function(d) { 
+				      	return x(d); 
+				      })
+				      .attr("width", x.rangeBand())
+				      .attr("y", function(d) { 
+				      	return y(rawData[d]['Total Campus']); })
+				      .attr("height", function(d) { 
+				      	return height - y(rawData[d]['Total Campus']); })
+				      .attr("fill", function(d) {
+				      	return "hsla(177,100%," + colorScale(rawData[d]['Total Campus']) + "%,1)";
+				      })
+				      .on("mouseover", tip.show)
+				      .on("mouseout", tip.hide);
 }
